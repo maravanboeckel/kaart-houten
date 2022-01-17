b488272f498de39b150ee07d8937b047fbcc9af9
maravanboeckel <89785847+maravanboeckel@users.noreply.github.com>
2022-01-17 20:55:56 +0100

    Update kaart_streamlit.py

--- kaart_streamlit.py
+++ kaart_streamlit.py
@@ -177,5 +177,5 @@ for row in vergelijk1.iterrows():
 legend_houten = add_categorical_legend(map_houten, 'Scheefstand',
                            colors=['darkred','red', 'orange', 'green'],
                            labels=['Meer dan 6°', 'Tussen 3° en 6°', 'Tussen 1° en 3°', 'Minder dan 1°'])
-folium_static(map_houten, width = 1000, height = 750)
+folium_static(map_houten)
 
